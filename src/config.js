8dd0672a2780874469ef894d2399112f1a8b2293
calumbutler <calum@gigantum.com>
2018-09-06 17:19:56 -0400

    added npm scripts and secret for deploy

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ const envHost = isWindows ? 'WINDOWS_HOST=1' : `LOCAL_USER_ID=${os.userInfo().ui
 
 // gigantum image name
 const imageLabel = 'gigantum/labmanager';
-const imageTag = '75efef53';
-const clientVersion = '0.9.4';
+const imageTag = '36e27262';
+const clientVersion = '0.10.0';
 
 //env constants
 const condaDir = "CONDA_DIR=/opt/conda";
@@ -64,6 +64,6 @@ export default {
     'releaseNotes',
     'failed',
   ],
-  fileSize: 244853862,
+  fileSize: 243665950,
   version,
 };
