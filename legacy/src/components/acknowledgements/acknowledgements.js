497676640432e483ae6826ece28bdcec4c231d65
Maz <mazharrr@live.com>
2019-08-07 11:50:15 -0400

    secrets removed

--- legacy/src/components/acknowledgements/acknowledgements.js
+++
@@ -1,8 +0,0 @@
-import marked from 'marked'
-import { ipcRenderer } from 'electron';
-
-
-ipcRenderer.on('file-open', (event, filedata) => {
-  document.getElementById('content').innerHTML = marked(filedata);
-});
-
