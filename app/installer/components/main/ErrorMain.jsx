f5a95da6b03263a59b93dc290472d224fd73a973
Maz <mazharrr@live.com>
2019-09-17 14:54:40 -0400

    error states

--- app/installer/components/main/ErrorMain.jsx
+++ app/installer/components/main/ErrorMain.jsx
@@ -1,25 +1,58 @@
 // @flow
 import React, { Component } from 'react';
+// constants
+import {
+  CONFIGURE_DOCKER,
+  CONFIGURE_GIGANTUM,
+  INSTALL_DOCKER,
+  ERROR
+} from '../../machine/InstallerConstants';
 // assets
 import './ErrorMain.scss';
 
-type Props = {};
+type Props = {
+  currentState: string
+};
 
 export default class ErrorMain extends Component<Props> {
   props: Props;
 
   render() {
-    return (
-      <div className="Layout__Main">
+    const { props } = this;
+
+    const renderMap = {
+      [CONFIGURE_DOCKER]: (
         <p>
-          To begin the configuration process you must have at least <b>8GB</b>{' '}
-          of disk space
+          There was an error configuring Docker. Please try again. If the issue
+          persists restart the application.
         </p>
+      ),
+      [CONFIGURE_GIGANTUM]: (
         <p>
-          This is required to make sure you have enough storage to start
-          working.
+          There was an error configuring Gigantum. Please try again. If the
+          issue persists restart the application.
         </p>
-      </div>
-    );
+      ),
+      [INSTALL_DOCKER]: (
+        <p>
+          There was an error installing docker. Please try again. If the issue
+          persists restart the application.
+        </p>
+      ),
+      [ERROR]: (
+        <div className="Layout__Main">
+          <p>
+            To begin the configuration process you must have at least <b>8GB</b>{' '}
+            of disk space
+          </p>
+          <p>
+            This is required to make sure you have enough storage to start
+            working.
+          </p>
+        </div>
+      )
+    };
+
+    return renderMap[props.currentState];
   }
 }
