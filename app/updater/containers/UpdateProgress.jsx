a3bac4ba877f14e8cb8a56b2bae1f5587e941821
Maz B <Mazharrr@users.noreply.github.com>
2021-03-24 10:22:44 -0400

    Download Update spinner (#181)
    * update progress
    
    * file paths

--- app/updater/containers/UpdateProgress.jsx
+++ app/updater/containers/UpdateProgress.jsx
@@ -1,9 +1,8 @@
 // @flow
 import React, { Component } from 'react';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
 // componenets
 import Header from '../components/Header';
+import CircularProgress from '../../installer/components/progressbar/circular/CircularProgress';
 // assets
 import './UpdateProgress.scss';
 
@@ -22,17 +21,13 @@ export default class UpdateProgress extends Component<Props> {
       <div className="UpdateProgress">
         <Header message={message} />
         <div className="UpdateProgress__body">
-          <CircularProgressbar
-            value={progress}
-            text={`${Math.floor(progress)}%`}
-            styles={buildStyles({
-              strokeLinecap: 'butt',
-              textSize: '24px',
-              textColor: '#9b9c9e',
-              trailColor: '#e3e4e5',
-              pathColor: '#386e80'
-            })}
-          />
+          {progress === 0 && <div className="UpdateProgress__spinner" />}
+          {progress > 0 && (
+            <CircularProgress
+              progress={progress}
+              text={`${Math.floor(progress)}%`}
+            />
+          )}
           <div className="UpdateProgress__message">
             Downloading Gigantum Desktop
           </div>
